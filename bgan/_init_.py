7e1f50388faaf71ea8ee9f94b3cd1bf9e3dcd7b6
Thomasdlsv <115259077+Thomasdlsv@users.noreply.github.com>
2025-04-06 13:43:03 +0200

    Update _init_.py

--- bgan/_init_.py
+++ bgan/_init_.py
@@ -1,10 +1,13 @@
-"""Synthesizers module."""
+# -*- coding: utf-8 -*-
 
+"""Top-level package for ctgan."""
+
+__author__ = 'DataCebo, Inc.'
+__email__ = 'info@sdv.dev'
+__version__ = '0.11.1.dev0'
+
+from ctgan.demo import load_demo
 from ctgan.synthesizers.ctgan import CTGAN
 from ctgan.synthesizers.tvae import TVAE
 
-__all__ = ('CTGAN', 'TVAE')
-
-
-def get_all_synthesizers():
-    return {name: globals()[name] for name in __all__}
+__all__ = ('CTGAN', 'TVAE', 'load_demo')
